af917893bad8ea97f49f86b7820ca769115e02ac
Hrishikesh Kolhe <hrishikeshkolhe24@gmail.com>
2025-01-23 12:52:00 +0530

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,39 @@
+import logo from "../assets/HrishiLogo.png";
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+
+const Navbar = () => {
+  return (
+    <nav className=" mb-20 flex items-center justify-between py-6">
+      <div className="flex flex-shrink-0 items-center">
+        <img className="" src={logo} alt="" />
+      </div>
+      <div className="m-8 flex items-center justify-center gap-4 text-2xl">
+        <a
+          href="https://www.linkedin.com/in/hrishikeshkolhe/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaLinkedin />
+        </a>
+        <a
+          href="https://github.com/hrishii24"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaGithub />
+        </a>
+        <a
+          href="https://www.instagram.com/mustbe_hrishi?igsh=Mmtobmx1N3ZjanJo"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaInstagram />
+        </a>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ src/constants/index.js
@@ -0,0 +1,63 @@
+import project1 from "../assets/projects/project-1.png";
+import project2 from "../assets/projects/project-2.png";
+import project3 from "../assets/projects/project-3.png";
+import project4 from "../assets/projects/project-4.png";
+import project5 from "../assets/projects/project-5.png";
+
+export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. With 2 years of hands-on experience, I have honed my skills in front-end technologies like React as well as back-end technologies like Node.js and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
+
+export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. With 2 years of professional experience, I have worked with a variety of technologies, including React.js, Node.js and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies.`;
+
+export const EXPERIENCES = [
+  {
+    year: "2023 - Present",
+    role: "Frontend Developer",
+    company: "Primal Infosys",
+    description: `Worked as a team member in developing and maintaining web applications using JavaScript, React.js, and Node.js. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with stakeholders to define project requirements and timelines.`,
+    technologies: ["Javascript", "Bootstrap", "React.js", "Node.js", "mongoDB"],
+  },
+];
+
+export const PROJECTS = [
+  {
+    title: "E-learning Website",
+    image: project1,
+    description:
+      "Designed and implemented an educational app with distinct Admin and Student roles, ensuring secure access and efficient course management. Developed React-based web applications with reusable components, collaborated on REST API integration, participated in Scrum meetings, and enhanced user experience using React-Router for seamless navigation.",
+    technologies: ["React", "Redux toolkit", "Node.js", "MongoDB"],
+  },
+  {
+    title: "Employee management system",
+    image: project2,
+    description:
+      "Developed an employee management system, enhancing communication between employees and management through email notifications. Led HTML and CSS development to meet design requirements, implemented responsive web strategies for cross-device compatibility, created reusable code components for future use, and conducted regular code reviews to ensure quality.",
+    technologies: ["React", "Node.js", "MongoDB"],
+  },
+  {
+    title: "Obys agency clone",
+    image: project3,
+    description:
+      "Created a responsive clone of the Obys agency website using HTML, CSS, and JavaScript. Focused on replicating the design, implementing smooth animations, and enhancing interactivity to provide a seamless user experience while improving front-end development skills and attention to detail.",
+    technologies: ["HTML", "CSS JavaScript"],
+  },
+  {
+    title: "Weather checking",
+    image: project4,
+    description:
+      "Developed a weather app using HTML, CSS, and JavaScript with a search feature that enables users to check real-time weather conditions of any city, focusing on API integration, responsive design, and user-friendly interface to enhance the overall user experience.",
+    technologies: ["HTML", "CSS", "JavaScript"],
+  },
+  {
+    title: "Personal portfolio",
+    image: project5,
+    description:
+      "Developed a personal portfolio website using React and Tailwind CSS, showcasing projects, skills, and experience. Focused on responsive design, smooth animations, and a clean UI to provide an engaging and professional online presence.",
+    technologies: ["React", "Tailwind css"],
+  },
+];
+
+export const CONTACT = {
+  address: "Navale residency, Pimple saudagar, Pune, 411027 ",
+  phoneNo: "+91 87967 02029",
+  email: "hrishikeshkolhe24@gmail.com",
+};
