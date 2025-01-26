8da2fe2ec5e9ef7471e2905ecf82e624cebe06b7
Hrishikesh Kolhe <hrishikeshkolhe24@gmail.com>
2025-01-26 16:48:41 +0530

    View tracker added

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
 import Technologies from "./components/Technologies";
+import { Analytics } from "@vercel/analytics/next";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
       </div>
       <div className="container mx-auto px-8">
+        <Analytics />
         <Navbar />
         <Hero />
         <About />
